3eb4e44c9e50ac2d39f11aff280a071d2507ca27
fikriwado <fixwad.online@gmail.com>
2024-03-24 14:43:13 +0700

    page home

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,10 +1,17 @@
 import { ReactNode } from 'react'
 import App from '../App'
+import { Col, Row } from 'antd'
 
 interface LayoutProps {
   children: ReactNode
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => <App>{children}</App>
+const Layout: React.FC<LayoutProps> = ({ children }) => (
+  <App>
+    <Row justify={'center'}>
+      <Col style={{ maxWidth: '1200px' }}>{children}</Col>
+    </Row>
+  </App>
+)
 
 export default Layout
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,54 @@
-import '../App.css'
+import { useEffect, useState } from 'react'
 import { Layout } from '../layouts'
-import { Button } from 'antd'
+import { Row, Col, Avatar, Card } from 'antd'
+import axios from 'axios'
 
-const Home = () => (
-  <Layout>
-    <Button type='primary'>Button asd</Button>
-  </Layout>
-)
+interface Post {
+  id: string
+  title: string
+  description: string
+  urlToImage: string
+}
+
+const Home = () => {
+  const [posts, setPosts] = useState<Post[]>([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const { data } = await axios(
+        `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=c898b261f2374ad7bec6a9ae7022e088`
+      )
+      setPosts(data.articles)
+    }
+    fetchPosts().then((r) => r)
+  }, [])
+
+  return (
+    <Layout>
+      <Row justify={'center'}>
+        <Col>
+          <div style={{ padding: '100px 50px' }}>
+            <h1 style={{ fontSize: '24px', fontWeight: 'bold' }}>EIGEN TEST</h1>
+          </div>
+        </Col>
+      </Row>
+      <Row gutter={[16, 24]}>
+        {posts.map((post) => (
+          <Col className='gutter-row' md={6}>
+            <Card cover={<img alt='example' src={post.urlToImage} />}>
+              <Card.Meta
+                avatar={
+                  <Avatar src='https://api.dicebear.com/7.x/miniavs/svg?seed=8' />
+                }
+                title={post.title}
+                description={post.description}
+              />
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Layout>
+  )
+}
 
 export default Home
